10673e424f9949520fc579ccb6c1bf23417fabae
valentine <valentin.paramonov@protonmail.com>
2017-08-10 13:40:08 +0300

    Pass channel to record as a command-line argument

--- twitch-live.py
+++ twitch-live.py
@@ -18,18 +18,19 @@ class Stopwatch:
         return now - last
 
 
-def record():
+def record(channel):
     downloaded = deque(maxlen=8)
     sleep_seconds = 10
     stopwatch = Stopwatch()
     while True:
         stopwatch.split()
-        segments = fetch_segments()
+        segments = fetch_segments(channel)
         new_segments = list(filter(lambda s: s.title not in downloaded, segments))
         write(new_segments)
         if segments_lost(downloaded, new_segments):
             sys.stderr.write('Lost segments detected!\n')
         for segment in new_segments:
+            print(segment.title)
             downloaded.append(segment.title)
         sleep_seconds = adjust_sleep(
             sleep_seconds,
@@ -40,14 +41,14 @@ def record():
             sleep(time_to_sleep)
 
 
-def fetch_segments():
+def fetch_segments(channel):
     token = requests.get(
-        'https://api.twitch.tv/api/channels/mega64podcast/access_token',
+        'https://api.twitch.tv/api/channels/{}/access_token'.format(channel),
         headers={'Client-ID': 'jzkbprff40iqj646a697cyrvl0zt2m6'}
     ).json()
 
     playlist = requests.get(
-        'https://usher.ttvnw.net/api/channel/hls/mega64podcast.m3u8',
+        'https://usher.ttvnw.net/api/channel/hls/{}.m3u8'.format(channel),
         params={'token': token['token'], 'sig': token['sig']}
     ).content.decode('utf-8')
 
@@ -72,6 +73,10 @@ def segments_lost(downloaded, new_segments):
         segment_index(new_segments[0].title) != segment_index(downloaded[-1]) + 1
 
 
+def segment_index(segment_title):
+    return int(segment_title.split('-')[1])
+
+
 def write(segments):
     with open('out.ts', 'ab') as file:
         for segment in segments:
@@ -93,9 +98,9 @@ def adjust_sleep(current_sleep, old_segment_count):
         return current_sleep + 0.5
 
 
-def segment_index(segment_title):
-    return int(segment_title.split('-')[1])
-
-
 if __name__ == '__main__':
-    record()
+    if (len(sys.argv)) != 2:
+        sys.stderr.write('Needs channel to record!\n')
+        sys.exit(1)
+    channel = sys.argv[1]
+    record(channel)
