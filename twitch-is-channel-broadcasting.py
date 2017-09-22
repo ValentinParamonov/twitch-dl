7324626e21bcf941cc549f440653bb770b846b58
valentine <valentin.paramonov@protonmail.com>
2017-09-22 11:48:45 +0300

    Add twitch-is-channel-broadcasting

--- twitch-is-channel-broadcasting.py
+++ twitch-is-channel-broadcasting.py
@@ -1,2 +1,13 @@
 #!/usr/bin/env python3
+import sys
+
+from twitch.playlist import Playlist
+
+if (len(sys.argv)) != 2:
+    sys.stderr.write('No channel name given!\n')
+    sys.exit(1)
+channel_name = sys.argv[1]
+playlist = Playlist.fetch_for_channel(channel_name)
+if playlist is None:
+    sys.exit(1)
 
