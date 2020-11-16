510038b922021af1ff4743ab84f8a66f97530201
valentine <valentin.paramonov@protonmail.com>
2020-11-16 16:45:22 +0200

    Remove unneeded parameters from links

--- twitch/constants.py
+++ twitch/constants.py
@@ -1,7 +1,7 @@
 class Twitch:
-    channel_playlist_link = 'https://usher.ttvnw.net/api/channel/hls/{}.m3u8?player_backend=mediaplayer'
+    channel_playlist_link = 'https://usher.ttvnw.net/api/channel/hls/{}.m3u8'
     vod_playlist_link = 'http://usher.justin.tv/vod/{}'
-    channel_token_link = 'https://api.twitch.tv/api/channels/{}/access_token?oauth_token=undefined&need_https=true&platform=web&player_type=site&player_backend=mediaplayer'
+    channel_token_link = 'https://api.twitch.tv/api/channels/{}/access_token'
     vod_token_link = 'https://api.twitch.tv/api/vods/{}/access_token'
     stream_link = 'https://api.twitch.tv/helix/streams'
     client_id_header = {'Client-ID': 'jzkbprff40iqj646a697cyrvl0zt2m6'}
