9be15404e6a8240228a4600859c137d5cbd30db1
valentine <valentin.paramonov@protonmail.com>
2020-11-18 23:31:00 +0200

    Persist the channel token

--- twitch/token.py
+++ twitch/token.py
@@ -1,25 +1,34 @@
 import json
+from os import path
 from time import time
 
 from twitch.constants import Twitch
 from util.contents import Contents
+from util.persistent_resource import PersistentJsonResource
 
 
 class Token:
-    __expiration_buffer_ms = 5000
+    __expiration_buffer_seconds = 5
+    __token_file_name = '~/.cache/twitch-dl/{}'
 
     def __init__(self):
-        self.__token = None
+        self.__token_resource = None
 
     def fetch_for_channel(self, channel_name):
-        if not self.__token or self.__is_expired(self.__token):
-            self.__token = self.__fetch(Twitch.channel_token_link.format(channel_name))
-        return self.__token
+        if not self.__token_resource:
+            token_file_name = path.expanduser(
+                self.__token_file_name.format(channel_name)
+            )
+            self.__token_resource = PersistentJsonResource(token_file_name)
+        if not self.__token_resource.value() or \
+                self.__is_expired(self.__token_resource.value()):
+            self.fetch_and_store_token(channel_name)
+        return self.__token_resource.value()
 
     @staticmethod
     def __is_expired(token):
         token_expiration_ms = json.loads(token['token'])['expires']
-        return token_expiration_ms - Token.__expiration_buffer_ms < time()
+        return token_expiration_ms + Token.__expiration_buffer_seconds < time()
 
     @classmethod
     def __fetch(cls, link):
@@ -28,6 +37,11 @@ class Token:
             headers=Twitch.client_id_header,
             onerror=lambda _: None
         )
+
+    def fetch_and_store_token(self, channel_name):
+        token = self.__fetch(Twitch.channel_token_link.format(channel_name))
+        if token:
+            self.__token_resource.store(token)
 
     @classmethod
     def fetch_for_vod(cls, vod_id):
---
+++ util/persistent_resource.py
@@ -0,0 +1,24 @@
+import json
+import os
+from os import path
+
+
+class PersistentJsonResource:
+    def __init__(self, file_name):
+        self.__file_name = file_name
+        self.__value = None
+
+    def value(self):
+        if self.__value:
+            return self.__value
+        if path.isfile(self.__file_name):
+            with open(self.__file_name, 'r') as resource_file:
+                return json.load(resource_file)
+        return None
+
+    def store(self, value):
+        if value:
+            os.makedirs(path.dirname(self.__file_name), exist_ok=True)
+            with open(self.__file_name, 'w') as resource_file:
+                json.dump(value, resource_file)
+            self.__value = value
